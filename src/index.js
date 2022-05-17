6b70454ef91fda9fa7fe7a77dc7b9e061f2617ed
HectorRC920 <hector.rubio.920@gmail.com>
2022-05-17 09:15:19 -0600

    Arquitectura de vistas y componentes con React Router DOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import reactDom from 'react-dom';
 
-import App from './components/App';
+import App from './routes/App';
 
 const root = reactDom.createRoot(document.getElementById('app'));
 root.render(<App />);
