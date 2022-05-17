39a992660b410ce70f059f5c99939864f461aec9
HectorRC920 <hector.rubio.920@gmail.com>
2022-05-17 09:43:45 -0600

    Maquetación y estilos del home

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="Footer-title">Platzi Conf Mex</p>
+      <p className="Footer-copy">Merequetengue</p>
+    </div>
+  );
+};
+
+export default Footer;
